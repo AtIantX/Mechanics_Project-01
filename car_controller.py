293a926b534b82d2affba5db1cbf51088313b3d7
AlantX <john.zohrabyan@gnmail.com>
2020-03-01 15:51:34 +0400

    Car Road Structure Created

---
+++ car_controller.py
@@ -0,0 +1,7 @@
+class CarController:
+    def __init__(self):
+        initial_speed_range = range(20, 81)
+        initial_distance = range(10, 151)
+        is_car_accelerating = True
+        car_acceleration_positive = range(1, 4)
+        car_acceleration_negative = range(1, 4)
---
+++ intersection_controller.py
@@ -0,0 +1,4 @@
+class IntersectionController:
+    def __init__(self):
+        intersection_width = range(5, 21)
+        yellow_light_duration = range(2, 6)
